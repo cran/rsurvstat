d0f792c26957abfc202d7703606b1a16d205a515
Robert Challen <rob.challen@bristol.ac.uk>
2026-01-17 11:50:02 +0000

    version 0.1.2

---
+++ R/globals.R
@@ -0,0 +1,28 @@
+utils::globalVariables(c(
+  "age_cat",
+  "age_start",
+  "age_end",
+  "age_groups",
+  "diseases",
+  "elapsed_week",
+  "value",
+  "week",
+  "year",
+  ":=",
+  "col_code",
+  "col_name",
+  "count",
+  "data",
+  "data2",
+  "disease_name",
+  "incid",
+  "model",
+  "new_data",
+  "population",
+  "row_code",
+  "row_name"
+))
+
+
+#' @importFrom sf vec_ptype2.sfc
+NULL
